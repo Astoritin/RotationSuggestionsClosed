5db580ba6feaddfa5efe5308538f2a90585c78e3
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-05-29 23:36:33 +0800

    Update action.sh

--- module/action.sh
+++ module/action.sh
@@ -2,13 +2,16 @@
 MODDIR=${0%/*}
 
 result_rs="$(settings get secure show_rotation_suggestions)"
-echo "- Current result of showing"
-echo "- rotation suggestion button: $result_rs"
 if [ $result_rs = 0 ]; then
-    echo "- Current state: disabled"
+    echo "- Current state: OFF"
     settings put secure show_rotation_suggestions 1
+    echo "- Button enabled"
+    echo "- Now button will be shown as detecting rotation"
 elif [ $result_rs = 1 ]; then
-    echo "- Current state: enabled"
+    echo "- Current state: ON"
     settings put secure show_rotation_suggestions 0
+    echo "- Button disabled"
+    echo "- Now button will NOT be shown as detecting rotation"
 fi
-echo "- Done"
+sleep 3
+exit 0
