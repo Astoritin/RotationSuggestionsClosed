c42b609b725d91ea0f4f3982ba4e3bee09059de1
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-05-29 23:37:12 +0800

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -5,7 +5,7 @@ VERIFY_DIR="$TMPDIR/.aa_verify"
 
 MOD_NAME="$(grep_prop name "${TMPDIR}/module.prop")"
 MOD_VER="$(grep_prop version "${TMPDIR}/module.prop") ($(grep_prop versionCode "${TMPDIR}/module.prop"))"
-MOD_INTRO="A Magisk module to disable useless rotation suggestion button as rotating screen each time."
+MOD_INTRO="A Magisk module to disable rotation suggestion button as rotating screen each time."
 
 [ ! -d "$VERIFY_DIR" ] && mkdir -p "$VERIFY_DIR"
 
@@ -13,14 +13,14 @@ if ! settings get secure show_rotation_suggestions &>/dev/null; then
     abort "Your ROM does NOT support rotation suggestion feature"
 fi
 
-echo "- Extract aautilities.sh"
-unzip -o "$ZIPFILE" 'aautilities.sh' -d "$TMPDIR" >&2
-if [ ! -f "$TMPDIR/aautilities.sh" ]; then
-  echo "! Failed to extract aautilities.sh!"
+echo "- Extract aa-util.sh"
+unzip -o "$ZIPFILE" 'aa-util.sh' -d "$TMPDIR" >&2
+if [ ! -f "$TMPDIR/aa-util.sh" ]; then
+  echo "! Failed to extract aa-util.sh!"
   abort "! This zip may be corrupted!"
 fi
 
-. "$TMPDIR/aautilities.sh"
+. "$TMPDIR/aa-util.sh"
 
 logowl "Setting up $MOD_NAME"
 logowl "Version: $MOD_VER"
@@ -28,9 +28,10 @@ install_env_check
 show_system_info
 logowl "Install from $ROOT_SOL app"
 logowl "Essential checks"
-extract "$ZIPFILE" 'aautilities.sh' "$VERIFY_DIR"
+extract "$ZIPFILE" 'aa-util.sh' "$VERIFY_DIR"
 extract "$ZIPFILE" 'customize.sh' "$VERIFY_DIR"
 logowl "Extract module files"
+extract "$ZIPFILE" 'aa-util.sh' "$MODPATH"
 extract "$ZIPFILE" 'module.prop' "$MODPATH"
 extract "$ZIPFILE" 'action.sh' "$MODPATH"
 extract "$ZIPFILE" 'service.sh' "$MODPATH"
@@ -38,5 +39,5 @@ extract "$ZIPFILE" 'uninstall.sh' "$MODPATH"
 logowl "Set permission"
 set_permission_recursive "$MODPATH" 0 0 0755 0644
 logowl "Welcome to use ${MOD_NAME}!"
-DESCRIPTION="[⏳Reboot to take effect.] $MOD_INTRO"
-update_config_value "description" "$DESCRIPTION" "$MODPATH/module.prop"
+DESCRIPTION="[✨Reboot to take effect.] $MOD_INTRO"
+update_config_var "description" "$DESCRIPTION" "$MODPATH/module.prop"
