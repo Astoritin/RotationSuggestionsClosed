3b0aaf33f844704558c3a9507715360654f07ea7
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-06-02 23:18:38 +0800

    Update action.sh

--- module/action.sh
+++ module/action.sh
@@ -1,19 +1,21 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
-echo "Rotation Suggestions Closed"
-echo "Stop showing rotation suggestions button as rotating screen"
+echo "- Rotation Suggestions Closed"
+echo " "
+echo "- Stop showing rotation suggestions"
+echo "- button as rotating screen"
 echo " "
 result_rs="$(settings get secure show_rotation_suggestions)"
 if [ $result_rs = 0 ]; then
     echo "- Current state: OFF"
     settings put secure show_rotation_suggestions 1
-    echo "- Button enabled"
+    echo "- Turn on suggestions button"
     echo "- Now button will be shown as detecting rotation"
 elif [ $result_rs = 1 ]; then
     echo "- Current state: ON"
     settings put secure show_rotation_suggestions 0
-    echo "- Button disabled"
+    echo "- Turn off suggestions button"
     echo "- Now button will NOT be shown as detecting rotation"
 fi
 sleep 1
