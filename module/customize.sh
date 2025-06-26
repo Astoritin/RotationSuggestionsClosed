8b2a86e8878a9433f2a8aaf77f1c83b573027ac4
Astoritin <TakasugiMerlan@outlook.com>
2025-06-26 23:32:53 +0800

    Cleanup unused VERIFY_DIR

--- module/customize.sh
+++ module/customize.sh
@@ -1,15 +1,11 @@
 #!/system/bin/sh
 SKIPUNZIP=1
 
-VERIFY_DIR="$TMPDIR/.aa_verify"
-
 MOD_NAME="$(grep_prop name "${TMPDIR}/module.prop")"
 MOD_VER="$(grep_prop version "${TMPDIR}/module.prop") ($(grep_prop versionCode "${TMPDIR}/module.prop"))"
 MOD_INTRO="Stop showing rotation suggestions button."
 
 [ "$(getprop ro.build.version.sdk)" -lt 28 ] && abort "Rotation suggestions feature is NOT supported!"
-
-[ ! -d "$VERIFY_DIR" ] && mkdir -p "$VERIFY_DIR"
 
 echo "- Extract aa-util.sh"
 unzip -o "$ZIPFILE" 'aa-util.sh' -d "$TMPDIR" >&2
@@ -26,8 +22,8 @@ install_env_check
 show_system_info
 logowl "Install from $ROOT_SOL app"
 logowl "Root: $ROOT_SOL_DETAIL"
-extract "$ZIPFILE" 'customize.sh' "$VERIFY_DIR"
-extract "$ZIPFILE" 'aa-util.sh' "$VERIFY_DIR"
+extract "$ZIPFILE" 'customize.sh' "$TMPDIR"
+extract "$ZIPFILE" 'aa-util.sh' "$TMPDIR"
 extract "$ZIPFILE" 'module.prop' "$MODPATH"
 extract "$ZIPFILE" 'action.sh' "$MODPATH"
 extract "$ZIPFILE" 'service.sh' "$MODPATH"
